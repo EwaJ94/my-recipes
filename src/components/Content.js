06cb1a734ddc8e99868f21669aaddd9772d4ae65
Ewa Jochymková <ewa.jochymkova@seznam.cz>
2024-05-26 20:27:18 +0200

    create main structure

---
+++ src/components/Content.js
@@ -0,0 +1,9 @@
+import "./Content.css"
+
+const Content = () => {
+    return <div>
+        
+      </div>
+  }
+  
+  export default Content
---
+++ src/components/Search.js
@@ -0,0 +1,9 @@
+import "./Search.css"
+
+const Search = () => {
+  return <div>
+      
+    </div>
+}
+
+export default Search
